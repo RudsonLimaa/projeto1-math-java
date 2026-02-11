105d92384725e75f9af3561759a68e1dc5db4f57
Rudson <limarudson295@gmail.com>
2026-02-11 19:59:40 -0300

    Refactor Main class and implement area calculation

--- estrutura_sequencial/src/estrutura_sequencial/Main.java
+++ estrutura_sequencial/src/estrutura_sequencial/Main.java
@@ -1,8 +1,20 @@
-public class program {
+import java.util.Scanner;
+
+public class teste {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 
-	
-	
+		double R, A, pi = 3.14159;
+		
+		R = sc.nextDouble();
+		
+		A = pi * R * R;
+		
+		System.out.printf("A=%.4f%n", A);
+		
+		sc.close();
 	}
+
 }
